d2c6f23e7b7389b48d67deb7bdd376f7cdd4bb01
Akshat Sanghvi <jakshat569@gmail.com>
2024-07-11 22:19:48 +0530

    Update configuration.py

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,6 +1,9 @@
+import os
 from cnnClassifier.constants import *
 from cnnClassifier.utils.helper import read_yaml, create_directories
-from cnnClassifier.entity.config_entity import (DataIngestionConfig, PrepareBaseModelConfig)
+from cnnClassifier.entity.config_entity import (DataIngestionConfig, 
+                                                PrepareBaseModelConfig, 
+                                                TrainingConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -41,3 +44,25 @@ class ConfigurationManager:
             params_weights=self.params.WEIGHTS,
             params_classes=self.params.CLASSES
         )
+    
+    def get_training_config(self) -> TrainingConfig:
+        training = self.config.training
+        prepare_base_model = self.config.prepare_base_model
+        params = self.params
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "Training")
+        create_directories([
+            Path(training.root_dir)
+        ])
+
+        training_config = TrainingConfig(
+            root_dir=Path(training.root_dir),
+            trained_model_path=Path(training.trained_model_path),
+            updated_base_model_path=Path(prepare_base_model.updated_base_model_path),
+            training_data=Path(training_data),
+            params_epochs=params.EPOCHS,
+            params_batch_size=params.BATCH_SIZE,
+            params_is_augmentation=params.AUGMENTATION,
+            params_image_size=params.IMAGE_SIZE
+        )
+
+        return training_config
