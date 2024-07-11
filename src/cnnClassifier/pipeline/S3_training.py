ed2d4f5659354b2097b7a305b3b4cd10a7311e8f
Akshat Sanghvi <jakshat569@gmail.com>
2024-07-11 22:19:58 +0530

    Create S3_training.py

---
+++ src/cnnClassifier/pipeline/S3_training.py
@@ -0,0 +1,18 @@
+from cnnClassifier import logger
+from cnnClassifier.components.training import Training
+from cnnClassifier.config.configuration import ConfigurationManager
+
+STAGE3 = "Training"
+
+class ModelTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        training_config = config.get_training_config()
+        training = Training(config=training_config)
+        training.get_base_model()
+        training.train_valid_generator()
+        training.train()
+        
